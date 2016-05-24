a348f2b4936d61df9f8b5d88b2b5bdbafb5c9afd
apple <sj@sj.local>
2016-05-24 22:21:20 +0800

    Initial Commit

---
+++ UseDatePickerToSetCountNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseDatePickerToSetCountNumber
+//
+//  Created by Sj on 16/5/24.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
